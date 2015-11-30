23d743dfe05dfef9a0d645f2a948d9994db85437
Artur Kowalczyk <artur.kowalczyk@sentaca.com>
2015-11-30 15:21:47 +0100

    Docs of status() method

--- RapifireMqttClient.h
+++ RapifireMqttClient.h
@@ -107,7 +107,18 @@ class RapifireMqttClient
     boolean loop();
 
     /** 
-     *  Provides the information about PubSubClient connection status.
+     *  Provides the information about PubSubClient connection status. The method can return one of the following values:
+     *  
+     *  #define MQTT_CONNECTION_TIMEOUT     -4
+     *  #define MQTT_CONNECTION_LOST        -3
+     *  #define MQTT_CONNECT_FAILED         -2
+     *  #define MQTT_DISCONNECTED           -1
+     *  #define MQTT_CONNECTED               0
+     *  #define MQTT_CONNECT_BAD_PROTOCOL    1
+     *  #define MQTT_CONNECT_BAD_CLIENT_ID   2
+     *  #define MQTT_CONNECT_UNAVAILABLE     3
+     *  #define MQTT_CONNECT_BAD_CREDENTIALS 4
+     *  #define MQTT_CONNECT_UNAUTHORIZED    5
      *  
      *  @returns PubSubClient connection status.
      */
